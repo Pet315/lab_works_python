e4d6ee0b9ab53b11806d917e9fe6d89f69b171e4
Petro Shpyliovyi <79025915+Pet315@users.noreply.github.com>
2021-09-23 01:58:22 +0300

    Update n3.py

--- n3.py
+++ n3.py
@@ -1,23 +1,19 @@
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-signs = ['-', '+']
+us_in = input("user_input: ")
+signs = "+-"
 
-def res(i):
-    if ((us_in[i] in signs and us_in[i+1] in signs) 
-    # '2++12--3'
-        or (us_in[i] not in numbers and us_in[i] not in signs)):
-    # 'hello+12'
-        return False, None
-    if i == len(us_in)-2:
-        if us_in[i+1] not in signs and us_in[i+1] not in numbers:
-            return False, None
-        else:
-            return True, eval(us_in)
-            # '1+2+4-2+5-1', '123'
-    return res(i+1)
+def recurs(x):
+    if us_in == "" or not us_in[0].isdigit() or not us_in[len(us_in) - 1].isdigit(): # digit should be in the beginning and in the end
+        return 0
+    elif not us_in[x].isdigit() and us_in[x] not in signs: # not sign and not digit
+        return 0
+    elif us_in[x] in signs and us_in[x+1] in signs: # 5++3---
+        return 0
+    if x == len(us_in) - 1:
+        return 1
+    return recurs(x+1)
 
-us_in = str(input("us_in: "))
-if (us_in == "") or (us_in[0] in signs) or (us_in[len(us_in) - 1] in signs):
-# ''
-    print((False, None))
-    exit(1)
-print("result:", res(0))
+
+if recurs(0) == 1:
+    print("True,", eval(us_in))
+else:
+    print("False, None")
